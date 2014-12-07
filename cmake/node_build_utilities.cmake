3b65e71ebf4df0419d0ff28146fb0980b73203ce
Jihoon Lee <jihoonlee.in@gmail.com>
2014-12-08 07:55:38 +0900

    updates

--- cmake/node_build_utilities.cmake
+++ cmake/node_build_utilities.cmake
@@ -2,13 +2,17 @@
 # NPM UPDATE #
 ##############
 
+set(NPM_UPDATE_BIN "/usr/local/bin/npm")
 
 macro(catkin_npm_update)
-    add_custom_target(update
-      COMMAND npm update
-      DEPENDS node_modules
-      COMMENT "Installing node dependencies..."
-      VERBATIM
-    )
+    message(STATUS "    ... Installing Node dependencies")
+    safe_execute_process(COMMAND  "${NPM_UPDATE_BIN} update")
+    message(STATUS "    ... Done..")
+
+#    add_custom_target(update
+#      COMMAND npm update
+#      DEPENDS node_modules
+#      COMMENT "Installing node dependencies..."
+#      VERBATIM
+#    )
 endmacro()
-                      
